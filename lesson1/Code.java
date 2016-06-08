d3df1740a87aea5c89533c4b113a4cec95bedc6c
ramv13 <ramsai.vellanki@gmail.com>
2016-06-07 22:55:54 -0400

    Completed printing code

---
+++ lesson1/Code.java
@@ -0,0 +1,28 @@
+/*
+ * This defines a package which is required for all Java programs. For our
+ * purposes, it is easiest to name the package com.idtech.<your name>. So
+ * the syntax will be 
+ *          package com.idtech.<your name>;
+ */
+package com.idtech.ram;
+
+/*
+ * This defines a class which we will discuss in a later lesson
+ */
+public class Printing {
+
+    /* 
+     * For all Java programs, the block of code within the curly braces below
+     * is the first thing that runs when the program is run. This is called the
+     * main method. The syntax is always 
+     *      public static void main(String[] args)
+     * followed by the curly braces.
+     */
+    public static void main(String[] args) {
+        // The following line of code prints the message "Hello World!" to the 
+        // screen. The double quotation marks around the Hello World! identify 
+        // the characters as text.
+        System.out.println("Hello world!");
+    }
+    
+}
