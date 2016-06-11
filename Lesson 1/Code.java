d893cb28d20879f99a995b805549d1001260c8b1
ramv13 <ramsai.vellanki@gmail.com>
2016-06-10 22:24:20 -0400

    Added user input

--- Lesson 1/Code.java
+++ Lesson 1/Code.java
@@ -1,3 +1,5 @@
+import java.util.Scanner; // this import statement enables us to use the Scanner
+
 /**
  * Code for Lesson 1: Printing
  *
@@ -17,6 +19,20 @@ public class Printing {
         // screen. The double quotation marks around the Hello World! identify 
         // the characters as text.
         System.out.println("Hello world!");
+        
+        // the following line prints an empty line
+        System.out.println();
+        
+        // the following line prints the message without a new line at the end
+        System.out.print("Enter your name: ");
+        
+        // The following lines are a bit more complicated and you will 
+        // understand this after reading the next couple of lessons.
+        // Here, the program reads in your name and then says hello back to you!
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        scanner.close();
+        System.out.println("Hello "+name+"!");
     }
     
 }
